1dbcd2ef8cd2c5b6cb1cdd2a352c78d70c6a3236
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-11 20:30:47 +0200

    understanding number types & number operators

---
+++ basicdatatypes/integers.nim
@@ -0,0 +1,11 @@
+let
+    a = 11
+    b = 4
+
+echo "a + b = ", a + b      # Sum
+echo "a - b = ", a - b      # Substraction
+echo "a * b = ", a * b      # Multiplication
+echo "a / b = ", a / b      # Division with full float number
+echo "a div b = ", a div b  # Division with only integer part
+echo "a mod b = ", a mod b  # Division integer remainder
+
