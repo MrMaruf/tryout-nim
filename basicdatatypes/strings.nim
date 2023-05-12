d4ea276d2da373f8c049b6542207a66c24dd2c9e
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-12 20:21:40 +0200

    Added a bit of string manipulation

--- basicdatatypes/strings.nim
+++ basicdatatypes/strings.nim
@@ -2,8 +2,12 @@ let
   m = "word"
   n = "A sentence with interpunction."
   o = "" #  "" for strings
+
+
+var
   p = "32"
   q = "!"
+  r = 'z'
 
 echo m, " ", n, o, p, q
 
@@ -11,3 +15,14 @@ let special_strings = "Hi!\nTabbing this \t \\this\\"
 echo special_strings
 let raw_string = r"Hi!\nTabbing this \t \\this\\"
 echo raw_string
+
+p.add("def")
+echo "p is now: ", p
+
+q.add(r)
+echo "q is now: ", q
+
+echo "concat: ", p & q
+
+echo "p is still: ", p
+echo "q is still: ", q
