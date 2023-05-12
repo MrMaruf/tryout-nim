969107d0c78d8d6611625440b291deec7a499c10
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-12 20:17:29 +0200

    Trying out strings

---
+++ basicdatatypes/strings.nim
@@ -0,0 +1,13 @@
+let
+  m = "word"
+  n = "A sentence with interpunction."
+  o = "" #  "" for strings
+  p = "32"
+  q = "!"
+
+echo m, " ", n, o, p, q
+
+let special_strings = "Hi!\nTabbing this \t \\this\\"
+echo special_strings
+let raw_string = r"Hi!\nTabbing this \t \\this\\"
+echo raw_string
