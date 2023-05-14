a301d971f07322991646d54d1d9230e6980f1554
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-14 11:40:53 +0200

    Trying out comparisons

---
+++ basicdatatypes/comparing.nim
@@ -0,0 +1,28 @@
+let
+  g = 31
+  h = 99
+
+echo "g is greater than h: ", g > h
+echo "g is smaller than h: ", g < h
+echo "g is equal to h: ", g == h
+echo "g is not equal to h: ", g != h
+echo "g is greater or equal to h: ", g >= h
+echo "g is smaller or equal to h: ", g <= h
+
+let
+  i = 'a'
+  j = 'd'
+  k = 'Z'
+
+echo i < j
+echo i < k  
+
+let
+  m = "axyb"
+  n = "axyz"
+  o = "ba"
+  p = "ba "
+
+echo m < n  
+echo n < o  
+echo o < p  
