c150e2f89e220a937d4eec5f70d8d7bf3b5345df
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-20 19:11:08 +0200

    Understanding indexing

---
+++ containers/indexing.nim
@@ -0,0 +1,7 @@
+let j = ['a', 'b', 'c', 'd', 'e']
+
+echo j[1]   # count from the start starting at index #0
+echo j[^1]  # count from the back starting at index #1
+
+echo j[0 .. 3] 
+echo j[0 ..< 3]
