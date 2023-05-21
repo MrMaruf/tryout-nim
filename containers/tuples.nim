70b872baafb41fe5853bedf2c6ed3f07c8de3713
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-21 09:02:41 +0200

    Understanding tuples

---
+++ containers/tuples.nim
@@ -0,0 +1,8 @@
+let n = ("Banana", 2, 'c')  
+echo n
+
+var o = (name: "Banana", weight: 2, rating: 'c')
+echo o
+o[1] = 7          # Can be edited by index  
+o.name = "Apple"  # Can be edited by key
+echo o
