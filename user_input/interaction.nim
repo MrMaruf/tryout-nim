f48fb35cdf08784ae652a3fb93f02e64733f241e
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-26 23:47:32 +0200

    learning: getting number inputs

--- user_input/interaction.nim
+++ user_input/interaction.nim
@@ -2,3 +2,12 @@ echo "Please enter your name:"
 let name = readLine(stdin)  
 
 echo "Hello ", name, ", nice to meet you!"
+
+import strutils
+
+echo "Please enter your year of birth:"
+let yearOfBirth = readLine(stdin).parseInt() 
+
+let age = 2018 - yearOfBirth
+
+echo "You are ", age, " years old."
