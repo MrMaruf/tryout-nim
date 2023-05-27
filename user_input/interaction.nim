56c791fa94ef3720a8726cb59cb19c147ca50758
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-27 08:17:45 +0200

    fix: updated current year

--- user_input/interaction.nim
+++ user_input/interaction.nim
@@ -8,6 +8,6 @@ import strutils
 echo "Please enter your year of birth:"
 let yearOfBirth = readLine(stdin).parseInt() 
 
-let age = 2018 - yearOfBirth
+let age = 2023 - yearOfBirth
 
 echo "You are ", age, " years old."
