75adf33fe6b05b5e39494f44d28bfd1554337606
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-29 18:47:46 +0200

    learning: started tutorial part 1

---
+++ tutorial/greetings.nim
@@ -0,0 +1,29 @@
+# This is a comment
+
+#[
+You can have any Nim code text commented
+out inside this with no indentation restrictions.
+        yes("May I ask a pointless question?")
+  #[
+     Note: these can be nested!!
+  ]#
+]#
+
+echo "What's your name? "
+var name: string = readLine(stdin)
+echo "Hi, ", name, "!"
+
+var
+  x, y: int
+  # a comment can occur here too
+  a, b, c: string
+
+# this statement will be explained later:
+from std/strutils import parseInt
+
+echo "A number please: "
+let n = parseInt(readLine(stdin))
+case n
+of 0..2, 4..7: echo "The number is in the set: {0, 1, 2, 4, 5, 6, 7}"
+of 3, 8: echo "The number is 3 or 8"
+else: discard
