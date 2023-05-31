e90b69f74c96efa7d75f06d302cb1a26b5e61134
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-31 21:38:21 +0200

    chore: cleaned up the file a bit

--- tutorial/for.nim
+++ tutorial/for.nim
@@ -5,8 +5,6 @@ for i in 0 ..< 10:
   echo i
 var s = "some string"
 for idx, c in s[0 .. ^1]:
-  echo idx, " ", c
-for idx, c in s[0 .. ^1]:
   echo idx, " ", c # ^1 is the last element, ^2 would be one before it, and so on
 for index, item in ["a","b"].pairs:
   echo item, " at index ", index
