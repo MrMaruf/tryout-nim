68ab893db80678ccca3d884bdc1dbc996964e737
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-05-31 21:38:51 +0200

    learning: understanding blocks better

---
+++ tutorial/break.nim
@@ -0,0 +1,15 @@
+block myblock:
+  echo "entering block"
+  while true:
+    echo "looping"
+    break # leaves the loop, but not the block
+  echo "still in block"
+echo "outside the block"
+echo ""
+block myblock2:
+  echo "entering block"
+  while true:
+    echo "looping"
+    break myblock2 # leaves the block (and the loop)
+  echo "still in block" # it won't be printed
+echo "outside the block"
